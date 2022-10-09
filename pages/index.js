ef29c2387d761be4dc60c171e25e226d8852f88e
中川健太郎 Kentaro NAKAGAWA <k.nakagawa@kuroco.jp>
2022-10-09 21:43:39 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import styles from "../styles/Home.module.css";
+import { client } from "../libs/client";
+import Link from "next/link";
+
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+  console.log("data", data.contents);
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
+
+export default function Home({ blog }) {
+  console.log("props", blog);
+  return (
+    <div>
+      {blog.map((blog) => (
+        <li key={blog.id}>
+          <Link href={`blog/${blog.id}`}>
+            <a href="">{blog.title}</a>
+          </Link>
+        </li>
+      ))}
+    </div>
+  );
+}
