9c09a033118a97ee57e8aea3014569d9d2733c3a
中川健太郎 Kentaro NAKAGAWA <k.nakagawa@kuroco.jp>
2022-10-11 08:44:10 +0900

    環境構築完了（typescript+eslint+prettier+tailwind）

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,29 @@
-import styles from "../styles/Home.module.css";
-import { client } from "../libs/client";
-import Link from "next/link";
+import Link from 'next/link'
+import { client } from '../libs/client'
+import styles from '../styles/Home.module.css'
 
 export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: "blog" });
-  console.log("data", data.contents);
+  const data = await client.get({ endpoint: 'blog' })
+  console.log('data', data.contents)
 
   return {
     props: {
       blog: data.contents,
     },
-  };
-};
+  }
+}
 
 export default function Home({ blog }) {
-  console.log("props", blog);
+  console.log('props', blog)
   return (
     <div>
       {blog.map((blog) => (
         <li key={blog.id}>
           <Link href={`blog/${blog.id}`}>
-            <a href="">{blog.title}</a>
+            <a href=''>{blog.title}</a>
           </Link>
         </li>
       ))}
     </div>
-  );
+  )
 }
